741ce436ee4732c75a2d3cba533f85176a40f618
George Hlongwane <georgehlongwane8@gmail.com>
2025-03-27 23:54:44 +0200

    Created models for for the product and the fake competitor

--- electronics/models.py
+++ electronics/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    actual_price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class FakeCompetitor(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="competitors")
+    name = models.CharField(max_length=255)
+    fake_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.fake_price}"
